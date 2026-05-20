717e4d71c1ee5daa8927526910382d0c727f17a1
hxscyprodz <hxscyprodz@gmail.com>
2026-05-20 23:18:36 +0200

    Add database connection retry

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -2,14 +2,22 @@ import config from "./env.config";
 import { connect } from "mongoose";
 import logger from "../services/logger";
 
-const connectToDatabase = async () => {
-  try {
-    const mongoURI = config.MONGO_URI;
-    await connect(mongoURI);
-    logger.info("Connected to MongoDB successfully");
-  } catch (error) {
-    logger.error("Error connecting to MongoDB:", error);
-    process.exit(1);
+const connectToDatabase = async (): Promise<void> => {
+  for (let attempt = 1; attempt <= config.RETRY_LIMIT; attempt++) {
+    try {
+      const mongoURI = config.MONGO_URI;
+      await connect(mongoURI);
+      logger.info("Connected to MongoDB successfully");
+      return; // Exit the function if connection is successful
+    } catch (error) {
+      logger.error(`Attempt ${attempt} - Error connecting to MongoDB:`, error);
+      if (attempt < config.RETRY_LIMIT) {
+        await new Promise((resolve) => setTimeout(resolve, config.RETRY_INTERVAL));
+      } else {
+        logger.error("Database connection failed. Exiting...");
+        process.exit(1);
+      }
+    }
   }
 };
 
