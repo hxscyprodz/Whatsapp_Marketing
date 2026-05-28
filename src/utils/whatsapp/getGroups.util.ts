6426cb788ea03325b845c2e9950982c87f80d1fd
hxscyprodz <hxscyprodz@gmail.com>
2026-05-28 22:20:32 +0200

    remove already exists log

--- src/utils/whatsapp/getGroups.util.ts
+++ src/utils/whatsapp/getGroups.util.ts
@@ -3,35 +3,43 @@ import logger from "../../services/logger";
 import GroupModel from "../../models/group.model";
 
 const getGroups = async () => {
-    const FLAG = "FETCH_GROUPS";
-    try{
-        logger.info(`${FLAG} - Fetching groups...`);
-        const chats = await client.getChats();
-        const groups = chats.filter(chat => chat.isGroup);
+  const FLAG = "FETCH_GROUPS";
+  try {
+    logger.info(`${FLAG} - Fetching groups...`);
+    const chats = await client.getChats();
+    const groups = chats.filter((chat) => chat.isGroup);
 
-        if(groups.length === 0) {
-            logger.info(`${FLAG} - No groups found.`);
-            return;
-        };
-        
-        groups.forEach(async (group) => {
-            const isExistingGroup = await GroupModel.findOne({ whatsappGroupId: group.id._serialized });
-            if(!isExistingGroup) {
-                const newGroup = new GroupModel({
-                    whatsappGroupId: group.id._serialized,
-                    name: group.name,
-                });
-                await newGroup.save()
-                    .then(() => logger.info(`${FLAG} - Group "${group.name}" saved to database.`))
-                    .catch((error) => logger.error(`${FLAG} - Error saving group "${group.name}":`, error));
-            } else {
-                logger.info(`${FLAG} - Group "${group.name}" already exists in the database.`);
-            }
+    if (groups.length === 0) {
+      logger.info(`${FLAG} - No groups found.`);
+      return;
+    }
+
+    groups.forEach(async (group) => {
+      const isExistingGroup = await GroupModel.findOne({
+        whatsappGroupId: group.id._serialized,
+      });
+      if (!isExistingGroup) {
+        const newGroup = new GroupModel({
+          whatsappGroupId: group.id._serialized,
+          name: group.name,
         });
-        logger.info(`${FLAG} - Successfully fetched ${groups.length} groups.`);
-    } catch(error) {
-        logger.error(`${FLAG} - Error fetching groups:`, error);
-    };
+        await newGroup
+          .save()
+          .then(() =>
+            logger.info(`${FLAG} - Group "${group.name}" saved to database.`),
+          )
+          .catch((error) =>
+            logger.error(
+              `${FLAG} - Error saving group "${group.name}":`,
+              error,
+            ),
+          );
+      }
+    });
+    logger.info(`${FLAG} - Successfully fetched ${groups.length} groups.`);
+  } catch (error) {
+    logger.error(`${FLAG} - Error fetching groups:`, error);
+  }
 };
 
 export default getGroups;
