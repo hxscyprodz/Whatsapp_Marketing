a9862434129b5ba042727eed83f17bf442d89247
hxscyprodz <hxscyprodz@gmail.com>
2026-05-28 22:10:09 +0200

    Add cron bug

--- src/utils/whatsapp/postToGroups.util.ts
+++ src/utils/whatsapp/postToGroups.util.ts
@@ -1,16 +1,22 @@
 import logger from "../../services/logger";
-import { client } from "../../libs/whatsaapweb.lib";
+import { client, MessageMedia } from "../../libs/whatsaapweb.lib";
+import { ISendToGroupParams } from "../../types/types";
 
-const sendMessageToGroup = async (groupId: string, message: string) => {
-    const FLAG = "SEND_MESSAGE_TO_GROUP";
-    try {
-        logger.info(`${FLAG} - Sending message to group ${groupId}: ${message}`);
-        const chatId = `${groupId}@g.us`;
-        await client.sendMessage(chatId, message);
-        logger.info(`${FLAG} - Message sent successfully to group ${groupId}.`);
-    } catch (error) {
-        logger.error(`${FLAG} - Error sending message to group ${groupId}:`, error);
-    };
+const sendMessageToGroup = async (params: ISendToGroupParams) => {
+  const { groupId, message, imageUrl } = params;
+  const FLAG = "SEND_MESSAGE_TO_GROUP";
+  try {
+    logger.info(`${FLAG} - Sending message to group ${groupId}: ${message}`);
+    const media = imageUrl ? await MessageMedia.fromUrl(imageUrl) : null;
+    await client.sendMessage(
+      groupId,
+      media ? media : message,
+      media ? { caption: message } : undefined,
+    );
+    logger.info(`${FLAG} - Message sent successfully to group ${groupId}.`);
+  } catch (error) {
+    logger.error(`${FLAG} - Error sending message to group ${groupId}:`, error);
+  }
 };
 
 export default sendMessageToGroup;
