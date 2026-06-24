982a3c6a57ff009d06b60dcfb325375c24e80c5a
hxscyprodz <hxscyprodz@gmail.com>
2026-06-24 16:59:20 +0200

    fix detached head after restart

--- src/libs/whatsaapweb.lib.ts
+++ src/libs/whatsaapweb.lib.ts
@@ -1,4 +1,5 @@
 import { Client, LocalAuth, MessageMedia } from "whatsapp-web.js";
+import logger from "../services/logger";
 
 const client = new Client({
     authStrategy: new LocalAuth(),
@@ -7,10 +8,37 @@ const client = new Client({
         args: [
             "--no-sandbox",
             "--disable-setuid-sandbox",
+            "--disable-dev-shm-usage",
+            "--disable-gpu",
+            "--disable-web-security",
+            "--disable-extensions",
         ],
-        executablePath: '/usr/bin/google-chrome'
+        executablePath: '/usr/bin/google-chrome',
+        timeout: 60000,
     },
 });
+
+const hanldeShutdown = async(signal: string) => {
+    logger.warn(`Received ${signal}. Shutting down...`);
+    try{
+        await client.destroy();
+        logger.warn('Client destroyed');
+        process.exit(0);
+    } catch(error: any) {
+        logger.error(`Error shutting down: ${error.message}`)
+    }
+}
+
+process.on('SIGINT', () => hanldeShutdown('SIGINT'));
+process.on('SIGTERM', () => hanldeShutdown('SIGTERM'));
+
+process.once('SIGUSR2', async() =>  {
+    logger.warn('Nodemon restart signal received. Restarting...');
+    try{
+        await client.destroy();
+    } catch{}
+    process.kill(process.pid, 'SIGUSR2');
+})
 
 export {
     client,
